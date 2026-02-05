c727782cce07c4492b2bf79555efa55575d51d54
v i p e r <91398017+vipertronnx@users.noreply.github.com>
2026-02-05 09:28:14 -0500

    initial set up, framework based off of MK IV system

---
+++ app.js
@@ -0,0 +1,79 @@
+// Express App + Socket.IO inits
+const express = require('express')
+const app = express();
+
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+const cors = require('cors')
+const favicon = require('serve-favicon')
+const path = require('path')
+
+// Chat Client inits
+const chat = require('./modules/chat')(io);
+
+/**
+ * Setup static directory
+ * and rendering engine
+ *
+ */
+app.use(express.static('./public'))
+app.set("view engine","ejs")
+app.use(cors())
+app.use(favicon(path.join(__dirname, '/public/assets/img/favicon.ico')))
+
+
+/**
+ * Home Page
+ *
+ */
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+
+/**
+ * Overlays
+ *
+ */
+// app.get('/', (req, res) => {
+//   res.render('.ejs')
+// })
+
+
+
+/**
+ * API Endpoints
+ *
+ */
+
+// app.post('/api/v1/text', express.json(), (req, res) => {
+//   io.emit('text-alert', { message: req.body.msg });
+//   res.sendStatus(200);
+// })
+
+// app.post('/api/v1/bg-alert', express.json(), (req, res) => {
+//   io.emit('bg-alert');
+//   res.sendStatus(200);
+// })
+
+
+
+/**
+ * 404 / All
+ *
+ */
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+
+
+/**
+ * Listen on port
+ *
+ */
+server.listen(5000, async () => {
+  console.log('server is listening on port 5000....')
+})
---
+++ modules/chat.js
@@ -0,0 +1,25 @@
+/*
+ * Get socket IO
+ *
+ */
+var io;
+module.exports = function(importIO) {
+    io = importIO;
+}
+
+/**
+ *  Connect to Chat
+ *
+ */
+const chat = async function() {
+
+    // Chat Client init
+    console.log('chat service initialized')
+  }
+
+
+/**
+ * Initialize the chat bot
+ *
+ */
+chat();
