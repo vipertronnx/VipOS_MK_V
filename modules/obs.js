83ce707f33da9f02675718d0e7698b1303a1bccf
v i p e r <91398017+vipertronnx@users.noreply.github.com>
2026-02-06 08:55:15 -0500

    OBS web socket module updates, auto attempts to reconnect if OBS ws not found

--- modules/obs.js
+++ modules/obs.js
@@ -1,8 +1,21 @@
 const { default: OBSWebSocket } = require('obs-websocket-js');
 const obs = new OBSWebSocket();
 
-const obsInit = async function() {
+// Retry interval for OBS connection attempts (in milliseconds)
+const obsRetryInterval = process.env.OBS_RECONNECT_RETRY_INTERVAL || 5000;
 
+// Function to connect to OBS with retry logic
+function obsConnect() {
+  obs.connect(process.env.OBS_ADDRESS, process.env.OBS_PASSWORD).then((info) => {
+    console.log('Connected and identified', info)
+  }, () => {
+    console.error('Error Connecting, will attempt to reconnect in', obsRetryInterval / 1000, 'seconds...')
+    setTimeout(obsConnect, obsRetryInterval);
+  });
+}
+
+// Initialize OBS connection and set up event listeners
+const obsInit = async function() {
   // Declare some events to listen for.
   obs.on('ConnectionOpened', () => {
     console.log('Connection Opened');
@@ -10,7 +23,7 @@ const obsInit = async function() {
 
   obs.on('Identified', () => {
     console.log('Identified, good to go!')
-    // Send some requests.
+    // Get Scene List on successful connection example
     // obs.call('GetSceneList').then((data) => {
     //   console.log('Scenes:', data);
     // });
@@ -21,16 +34,13 @@ const obsInit = async function() {
     console.log(`Scene Changed to: ${data.sceneName}`);
   });
 
-  obs.connect(process.env.OBS_ADDRESS, process.env.OBS_PASSWORD).then((info) => {
-    console.log('Connected and identified', info)
-  }, () => {
-    console.error('Error Connecting')
-  });
-
+  obsConnect();
 }
 
+// Start the OBS connection process
 obsInit();
 
+// Export the obs instance for use in other modules
 module.exports = {
   obs: obs
 }
